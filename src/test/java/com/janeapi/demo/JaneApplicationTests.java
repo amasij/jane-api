731f7e6893f620fae56e26c7ae1b6ac203ce2758
simon <sjosephi@byteworks.com.ng>
2021-07-22 05:21:16 +0100

    first commit

---
+++ src/test/java/com/janeapi/demo/JaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janeapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
